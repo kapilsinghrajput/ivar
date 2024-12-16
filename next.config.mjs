f13c81aacd2edc30304c436496f98f5a11836b50
webepower <45253630+seven-webepower@users.noreply.github.com>
2024-12-16 10:54:06 +0530

    chnage next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'nodejs', // Ensures you're using a Node.js runtime instead of Edge.
-  },
+
     images: {
         remotePatterns: [
             {
